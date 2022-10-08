bcc683415c9013568e835ff58345563f97fc1b5c
Metu-O <70252129+Metu-O@users.noreply.github.com>
2022-10-08 16:04:38 -0500

    Add files via upload

---
+++ Evaluate_Method_Accuracy.py
@@ -0,0 +1,182 @@
+# Run this code in the qiime2-2022.2 environment.
+
+# Import modules and packages
+get_ipython().run_line_magic('matplotlib', 'inline')
+import os
+from os.path import join, exists, expandvars
+import pandas as pd
+from IPython.display import display, Markdown
+import seaborn
+import argparse
+from tax_credit.plotting_functions import (pointplot_from_data_frame,
+                                           boxplot_from_data_frame,
+                                           heatmap_from_data_frame,
+                                           per_level_kruskal_wallis,
+                                           beta_diversity_pcoa,
+                                           average_distance_boxplots,
+                                           rank_optimized_method_performance_by_dataset)
+from tax_credit.eval_framework import (evaluate_results,
+                                       method_by_dataset_a1,
+                                       parameter_comparisons,
+                                       merge_expected_and_observed_tables,
+                                       filter_df)
+
+def evaluate_method_accuracy(project_dir,expected_results_dir, mock_results_fp,results_dirs,mock_dir,min_count,taxonomy_level_range,outdir):
+#Experienced users can create their own path.  
+    project_dir = expandvars('$HOME/tax-credit-data/')
+    expected_results_dir = join(project_dir, "data/precomputed-results/", "mock-community")
+    mock_results_fp = join(expected_results_dir, 'mock_results.tsv')
+    results_dirs = [expected_results_dir]
+    mock_dir = join(project_dir, "data", "mock-community")
+    min_count = 1
+    taxonomy_level_range = range(2,7)
+    outdir = join(project_dir, "plots")
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    dataset_ids = ['mock-1', 'mock-2', 'mock-3', 'mock-4', 'mock-5','mock-6', 'mock-7', 'mock-8','mock-12','mock-13','mock-14', 'mock-15', 'mock-16','mock-18', 'mock-19', 'mock-20', 'mock-21', 'mock-22']
+    method_ids = ['q2-NB', 'q2-SFM-RF', 'q2-SFM-SDG','q2-SFM-NB']
+    ref_ids = ['gg_13_8_otus']
+    # Find mock community pre-computed tables, expected tables, and "query" tables
+    # Note: if you have added additional methods to add, set append=True. If you are attempting to recompute pre-computed results, set force=True.
+    mock_results = evaluate_results(results_dirs, 
+                                expected_results_dir, 
+                                mock_results_fp, 
+                                mock_dir,
+                                taxonomy_level_range=range(2,7), 
+                                min_count=min_count,
+                                taxa_to_keep=None, 
+                                md_key='taxonomy', 
+                                subsample=False,
+                                per_seq_precision=True,
+                                exclude=['other'],
+                                dataset_ids=dataset_ids,
+                                reference_ids=ref_ids,
+                                method_ids=method_ids,
+                                append=False,
+                                force=False, #force=True the first time 
+                                backup=False)
+    #Compute and summarize precision, recall, and F-measure for mock communities
+    color_palette={ 
+    'q2-NB':'black',
+    'q2-SFM-RF':'darkgreen',
+    'q2-SFM-SDG':'red',
+    'q2-SFM-NB':'blue',
+    }
+    y_vars = ["Precision", "Recall", "F-measure", "Taxon Accuracy Rate", "Taxon Detection Rate"]
+    point = pointplot_from_data_frame(mock_results, "Level", y_vars, 
+                                  group_by="Reference", color_by="Method",
+                                  color_palette=color_palette)
+    for k, v in point.items():
+        v.savefig(join(outdir, 'mock-{0}-lineplots.pdf'.format(k)))
+
+
+    # Heatmaps show the performance of individual method/parameter combinations at each taxonomic level, in each reference database
+    heatmap_from_data_frame(mock_results, metric="Precision", rows=["Method", "Parameters"], cols=["Reference","Level"])
+    plt = plt.figure(figsize=(24, 8))
+    plt.savefig(join(outdir, "Precision-heatmap")
+    heatmap_from_data_frame(mock_results, metric="Recall", rows=["Method", "Parameters"], cols=["Reference","Level"])
+    plt = plt.figure(figsize=(24, 8))
+    plt.savefig(join(outdir, "Recall-heatmap")
+    heatmap_from_data_frame(mock_results, metric="F-measure", rows=["Method", "Parameters"], cols=["Reference","Level"])
+    plt = plt.figure(figsize=(24, 8))
+    plt.savefig(join(outdir, "F-measure-heatmap")
+    heatmap_from_data_frame(mock_results, metric="Taxon Accuracy Rate", rows=["Method", "Parameters"], cols=    
+                            ["Reference", "Level"])
+    plt = plt.figure(figsize=(24, 8))
+    plt.savefig(join(outdir, "Taxon Accuracy Rate")
+    heatmap_from_data_frame(mock_results, metric="Taxon Detection Rate", rows=["Method", "Parameters"], cols=
+                            ["Reference", "Level"])
+    plt = plt.figure(figsize=(24, 8))
+    plt.savefig(join(outdir, "Taxon Detection Rate-heatmap")
+
+    #Now we will focus on results at species level (for genus level, change to level 5)
+    # Method optimization
+    # Which method/parameter configuration performed "best" for a given score? We can rank the top-performing configuration by dataset, method,    
+    #and taxonomic level. 
+    # First, the top-performing method/configuration combination by dataset.
+    mock_results_6 = mock_results[mock_results['Level'] == 6]
+    pd.set_option('display.max_colwidth', None)
+    for dataset in mock_results_6['Dataset'].unique():
+        display(Markdown('## {0}'.format(dataset)))
+        best = method_by_dataset_a1(mock_results_6, dataset)
+        display(best)
+        plt = plt.figure(figsize=(24, 8))
+        plt.savefig(join(outdir, "best_method")
+    
+    # Now we can determine which parameter configuration performed best for each method. Count best values in each     #column indicate how many   
+    #samples a given method achieved within one mean absolute deviation of the best      
+    #result (which is why they may sum to more than the total number of samples).
+    for method in mock_results_6['Method'].unique():
+        top_params = parameter_comparisons(
+        mock_results_6, method, 
+        metrics=['Taxon Accuracy Rate', 'Taxon Detection Rate', 'Precision', 'Recall', 'F-measure'])
+        display(Markdown('## {0}'.format(method)))
+        display(top_params[:5])
+        plt = plt.figure(figsize=(24, 8))
+        plt.savefig(join(outdir, "top_params")
+    
+    
+    # Optimized method performance
+    # And, finally, which method performed best at each individual taxonomic level for each reference dataset (i.e., for across all fungal and
+    #bacterial mock communities combined)? 
+    # For this analysis, we rank the top-performing method/parameter combination for each method at family through species levels. Methods are ranked 
+    #by top F-measure, and the average value for each metric is shown (rather than count best as above). F-measure distributions are plotted for
+    #each method, and compared using paired t-tests with FDR-corrected P-values. This cell does not need to be altered, unless if you wish to 
+    #change the metric used for sorting best methods and for plotting.
+    
+    boxes = rank_optimized_method_performance_by_dataset(mock_results,
+                                                     dataset="Reference",
+                                                     metric="F-measure",
+                                                     level_range=range(4,7),
+                                                     display_fields=["Method",
+                                                                     "Parameters",
+                                                                     "Taxon Accuracy Rate",
+                                                                     "Taxon Detection Rate",
+                                                                     "Precision",
+                                                                     "Recall",
+                                                                     "F-measure"],
+                                                     paired=True,
+                                                     parametric=True,
+                                                     color=None,
+                                                     color_palette=color_palette)
+    for k, v in boxes.items():
+    v.get_figure().savefig(join(outdir, 'mock-fmeasure-{0}-boxplots.pdf'.format(k)))
+
+def main():
+    parser = argparse.ArgumentParser(
+            description= 'The tax-credit-data directorsy contains all data used to prepare these codes. I am                   argparse to soft code important file paths needed to run this script. Experienced users can create                 their own paths')
+
+    req = parser.add_argument_group('REQUIRED')
+    req.add_argument('-r', '--project_dir', required=True, action='store',
+             help='Project_dir should be tax-credit-data directory')
+    req.add_argument('-e', '--expected_results_dir', required=True, action='store',
+             help='expected_results_dir contains expected composition                                                                data in the structure expected_results_dir/<dataset name>/<reference name>/expected/')
+    req.add_argument('-m', '--mock_results_fp', required=True, action='store',
+             type=int, help='mock_results_fp designates the files to which summary results are written. If this                file exists, it can be read in to generate results plots, instead of computing new scores.')
+    req.add_argument('-s', '--results_dirs', required=True, action='store',
+             type=int, help='results_dirs should contain the directory or directories where results can be found.                              By default, this is the same location as expected results included with the project.                              If other results should be included, absolute paths to those directories should be                                added to this list.')
+    req.add_argument('-o', '--mock_dir', required=True, action='store',
+             type=int, help='directory containing mock community data, e.g., feature table without taxonomy')
+    req.add_argument('-i', '--min_count', required=True, action='store',
+             type=int, help='Minimum number of times an OTU must be observed for it to be included in analyses.                                Edit this to analyze the effect of the minimum count on taxonomic results.')
+    req.add_argument('-t', '--taxonomy_level_range', required=True, action='store',
+             type=int, help='Define the range of taxonomic levels over which to compute accuracy scores. The                                   default given below will compute order (level 2) through species (level 6)')
+    req.add_argument('-u', '--outdir', required=True, action='store',
+             type=int, help='you can save plots in this directory')
+    
+    
+    p = parser.parse_args()
+    project_dir = p.project_dir
+    expected_results_dir = p.expected_results_dir
+    mock_results_fp  = p.mock_results_fp 
+    results_dirs =p.results_dirs
+    mock_dir = p.mock_dir
+    min_count = p.min_count
+    taxonomy_level_range = p.taxonomy_level_range
+    outdir = p.outdir
+    
+    evaluate_method_accuracy(project_dir,expected_results_dir, mock_results_fp,results_dirs,mock_dir,min_count,taxonomy_level_range,outdir)
+
+if __name__ == '__main__':
+        main()
---
+++ Naive_Bayes_Parameters.py
@@ -0,0 +1,239 @@
+# Run this notebook in the qiime2-2022.2 environment
+
+# Import modules and packages
+import os
+from os.path import join, exists, split, sep, expandvars 
+from os import makedirs, getpid
+from glob import glob
+from shutil import rmtree
+import csv
+import json
+import tempfile
+from itertools import product
+import argparse
+import pathlib
+from qiime2.plugins import feature_classifier
+from qiime2 import Artifact
+from joblib import Parallel, delayed
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from q2_feature_classifier.classifier import spec_from_pipeline
+from sklearn.feature_extraction.text import HashingVectorizer
+from q2_types.feature_data import DNAIterator
+from qiime2.plugin import Int, Str, Float, Bool, Choices, Range
+from q2_types.feature_table import FeatureTable, RelativeFrequency
+from q2_types.feature_data._transformer import (_read_from_fasta, _taxonomy_formats_to_dataframe, 
+                                                _fastaformats_to_series)
+from q2_feature_classifier._skl import _extract_reads, _specific_fitters, fit_pipeline
+from q2_types.feature_data import (TSVTaxonomyFormat, HeaderlessTSVTaxonomyFormat, 
+                                   FeatureData, Taxonomy, Sequence, DNAIterator, 
+                                   DNAFASTAFormat)
+from q2_feature_classifier.classifier import _load_class, spec_from_pipeline, pipeline_from_spec
+from pandas import DataFrame
+
+from tax_credit.framework_functions import (
+    gen_param_sweep, generate_per_method_biom_tables, move_results_to_repository)
+
+# The below methods are used to load the data, prepare the data, parse the classifier and classification parameters, and fit and run the classifier.
+
+def train_and_run_classifier(method_parameters_combinations, reference_dbs,
+                             pipelines, sweep, verbose=False, n_jobs=4):
+    '''Train and run q2-feature-classifier across a parameter sweep.
+    method_parameters_combinations: dict of dicts of lists
+        Classifier methods to run and their parameters/values to sweep
+        Format: {method_name: {'parameter_name': [parameter_values]}}
+    reference_dbs: dict of tuples
+        Reference databases to use for classifier training.
+        Format: {database_name: (ref_seqs, ref_taxonomy)}
+    pipelines: dict
+        Classifier pipelines to use for training each method.
+        Format: {method_name: sklearn.pipeline.Pipeline}
+    sweep: list of tuples
+        output of gen_param_sweep(), format:
+        (parameter_output_dir, input_dir, reference_seqs, reference_tax, method, params)
+    n_jobs: number of jobs to run in parallel.
+    '''
+    # train classifier once for each pipeline param combo
+    for method, db, pipeline_param, subsweep in generate_pipeline_sweep(
+            method_parameters_combinations, reference_dbs, sweep):
+        ref_reads, ref_taxa = reference_dbs[db]
+        # train classifier
+        classifier = train_classifier(
+            ref_reads, ref_taxa, pipeline_param, pipelines[method], verbose=verbose)
+        # run classifier. Only run in parallel once classifier is trained,
+        # to minimize memory usage (don't want to train large refs in parallel)
+        Parallel(n_jobs=n_jobs)(delayed(run_classifier)(
+            classifier, output_dir, input_dir, split_params(params)[0], verbose=verbose)
+            for output_dir, input_dir, rs, rt, mt, params in subsweep)
+        
+def generate_pipeline_sweep(method_parameters_combinations, reference_dbs, sweep):
+    '''Generate pipeline parameters for each classifier training step'''
+    # iterate over parameters
+    for method, params in method_parameters_combinations.items():
+        # split out pipeline parameters
+        classifier_params, pipeline_params = split_params(params)
+        # iterate over reference dbs
+        for db, refs in reference_dbs.items():
+            # iterate over all pipeline parameter combinations
+            for param_product in product(*[params[id_] for id_ in pipeline_params]):
+                # yield parameter combinations to use for a each classifier
+                pipeline_param = dict(zip(pipeline_params, param_product))
+                subsweep = [p for p in sweep if split_params(p[5])[1] 
+                            == pipeline_param and p[2] == refs[0]]
+                yield method, db, pipeline_param, subsweep
+
+def train_classifier(ref_reads, ref_taxa, params, pipeline, verbose=False):
+    ref_reads = Artifact.load(ref_reads)
+    ref_taxa = Artifact.load(ref_taxa)
+    pipeline.set_params(**params)
+    spec = json.dumps(spec_from_pipeline(pipeline))
+    if verbose:
+        print(spec)
+    classifier = feature_classifier.methods.fit_classifier_sklearn(ref_reads, ref_taxa, spec)
+    return classifier.classifier
+
+def run_classifier(classifier, output_dir, input_dir, params, verbose=False):    
+    # Classify the sequences
+    rep_seqs = Artifact.load(join(input_dir, 'rep_seqs.qza'))
+    if verbose:
+        print(output_dir)
+    classification = feature_classifier.methods.classify_sklearn(rep_seqs, classifier, **params)
+    
+    # Save the results
+    makedirs(output_dir, exist_ok=True)
+    output_file = join(output_dir, 'taxonomy.tsv')
+    dataframe = classification.classification.view(DataFrame)
+    dataframe.to_csv(output_file, sep='\t', header=False)
+    
+def split_params(params):
+    classifier_params = feature_classifier.methods.classify_sklearn.signature.parameters.keys()
+    pipeline_params = {k:v for k, v in params.items()
+                        if k not in classifier_params}
+    classifier_params = {k:v for k, v in params.items() 
+                         if k in classifier_params}
+    return classifier_params, pipeline_params
+
+
+def build_pipeline(classifier, hash_params, classify_params):
+    return Pipeline([
+        ('feat_ext', HashingVectorizer(**hash_params)),
+        ('classify', classifier(**classify_params))])
+
+def  main_wrapper_function(project_dir,analysis_name,data_dir,precomputed_dir,results_dir,reference_database_dir):
+    #Experienced users can create their own path.  
+    project_dir = expandvars('$HOME/tax-credit-data/')
+    analysis_name = 'mock-community'
+    data_dir = join(project_dir, 'data', analysis_name)
+    precomputed_dir = join(project_dir, 'data', 'precomputed-results', analysis_name)
+    results_dir = join(project_dir, 'temp_results_narrow')
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    reference_database_dir = join(project_dir, 'data','ref_dbs')
+    # Preparing the method/parameter combinations and generating commands
+    # Now we set the methods and method-specific parameters that we want to sweep. 
+    # Modify to sweep other methods.
+    dataset_reference_combinations = [
+        ('mock-1', 'gg_13_8_otus'), 
+        ('mock-2', 'gg_13_8_otus'), 
+        ('mock-3', 'gg_13_8_otus'), 
+        ('mock-4', 'gg_13_8_otus'), 
+        ('mock-5', 'gg_13_8_otus'), 
+        ('mock-6', 'gg_13_8_otus'), 
+        ('mock-7', 'gg_13_8_otus'), 
+        ('mock-8', 'gg_13_8_otus'),
+        ('mock-12', 'gg_13_8_otus'),
+        ('mock-13', 'gg_13_8_otus'),
+        ('mock-14', 'gg_13_8_otus'),
+        ('mock-15', 'gg_13_8_otus'),
+        ('mock-16', 'gg_13_8_otus'),
+        ('mock-18', 'gg_13_8_otus'),
+        ('mock-19', 'gg_13_8_otus'),
+        ('mock-20', 'gg_13_8_otus'),
+        ('mock-21', 'gg_13_8_otus'),
+        ('mock-22', 'gg_13_8_otus')]
+    
+    method_parameters_combinations = {
+    'q2-NB': {'confidence': [0.7],
+              'classify__alpha': [0.001],
+              'feat_ext__n_features':[2000, 4000, 6000, 8000],
+              'feat_ext__ngram_range': [[8,8]]}}
+    
+    # Test - to test subset of dataset
+    #dataset_reference_combinations = [
+    # ('mock-1', 'gg_13_8_otus'), # formerly S16S-1
+    #]
+    #
+    #method_parameters_combinations = {
+    #   'q2-NB': {'confidence': [0.7],
+    #            'classify__alpha': [0.001],
+    #           'feat_ext__n_features':[8000],
+    #           'feat_ext__ngram_range': [[8,8]]}}
+    
+    reference_dbs = {'gg_13_8_otus' : (join(reference_database_dir, 'gg_13_8_otus/99_otus_v4.qza'), 
+    join(reference_database_dir, 'gg_13_8_otus/99_otu_taxonomy_clean.tsv.qza'))}
+    
+    
+    # Preparing the pipelines
+    # pipeline params common to all classifiers are set here
+    
+    
+    hash_params = dict(
+        analyzer='char_wb',ngram_range=[8, 8],alternate_sign=False)
+    # any params common to all classifiers can be set here
+    classify_params = dict()
+    # Now fit the pipelines.
+    pipelines = {'q2-NB': build_pipeline(
+                 MultinomialNB, hash_params, classify_params)}
+    #Do the Sweep
+    sweep = gen_param_sweep(data_dir, results_dir, reference_dbs,
+                        dataset_reference_combinations,
+                        method_parameters_combinations)
+    sweep = list(sweep)
+    # A quick sanity check 
+    print(len(sweep))
+    sweep[0]
+
+    train_and_run_classifier(method_parameters_combinations, reference_dbs, pipelines, sweep, verbose=True, 
+                             n_jobs=1)
+
+    # Generate per-method biom tables. 
+    # Don't change this unless filepaths were altered in the preceding cells.
+    taxonomy_glob = join(results_dir, 'mock-*', 'gg_13_8_otus', 'q2-NB', '*', 'taxonomy.tsv')
+    generate_per_method_biom_tables(taxonomy_glob, data_dir)
+
+    # Move result files to tax-credit-data repository (to push these results to the repository or compare with   
+    #other precomputed results).
+    # Don't change this unless filepaths were altered in the preceding cells.
+    precomputed_results_dir = join(project_dir, "data", "precomputed-results", analysis_name)
+    method_dirs = glob(join(results_dir, '*', '*', '*', '*'))
+    move_results_to_repository(method_dirs, precomputed_results_dir)
+
+def main():
+    parser = argparse.ArgumentParser(
+            description= 'The tax-credit-data directorsy contains all data used to prepare these codes. I use argparse to soft code important file paths needed to run this script. Experienced users can create their own paths')
+
+    req = parser.add_argument_group('REQUIRED')
+    req.add_argument('-r', '--project_dir', required=True, action='store', type=pathlib.Path,
+             help='Project_dir should be tax-credit-data directory')
+    req.add_argument('-a', '--analysis_name', required=True, action='store', type=pathlib.Path,
+             help='Analysis_name is called mock-community consisting of prepared mock communities')
+    req.add_argument('-d', '--data_dir', required=True, action='store', type=pathlib.Path,
+             help='Data_dir points to prepared data in the structure <tax-credit-data>/<data>/<analysis_name>/')
+    req.add_argument('-e', '--precomputed_dir', required=True, action='store', type=pathlib.Path,
+             help='Precomputed_dir points to precomputed results in the stucture <tax-credit-data>/<data>/<precomputer_results>/<analysis_name>/')
+    req.add_argument('-s', '--results_dir', required=True, action='store', type=pathlib.Path,
+             help='Results_dir temporary stores results')
+    req.add_argument('-f', '--reference_database_dir', required=True, action='store', type=pathlib.Path,
+             help='Reference_database_dir points to sequence and taxonomy data in the stucture <tax-credit-data>/<data>/<ref_dbs>/')
+    p = parser.parse_args()
+    project_dir = p.project_dir
+    analysis_name = p.analysis_name
+    data_dir = p.data_dir
+    precomputed_dir =p.precomputed_dir
+    results_dir = p.results_dir
+    reference_database_dir = p.reference_database_dir
+    main_wrapper_function(project_dir,analysis_name,data_dir,precomputed_dir,results_dir,reference_database_dir)
+
+if __name__ == '__main__':
+        main()
