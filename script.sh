5265c90bbdf64b5d9ee19cac47bab63873bf6442
Metu-O <70252129+Metu-O@users.noreply.github.com>
2022-10-09 09:41:08 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -61,5 +61,11 @@ ipython Evaluate_Method_Accuracy.py\
   -t taxonomy_level_range\
   -u outdir
   
-  
-echo "I'm done"
+format_time() {
+  ((h=${1}/3600))
+  ((m=(${1}%3600)/60))
+  ((s=${1}%60))
+  printf "%02d:%02d:%02d\n" $h $m $s
+ }
+ 
+echo "Script completed in $(format_time $SECONDS)"
