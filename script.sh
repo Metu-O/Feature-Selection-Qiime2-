0d10205cda92ebaa8464d9533b14f240ca9b79dd
Metu-O <70252129+Metu-O@users.noreply.github.com>
2022-10-10 10:10:18 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,27 @@
 #SBATCH --mem-per-cpu=30Gb
 #SBATCH --output=metu_trial.out
 #SBATCH --time=60:00:00
+ 
+source /opt/conda/etc/profile.d/conda.sh
+conda activate /home/SE/BMIG-6202-MSR/qiime2-2022.2
+
+wd="/scratch/metu/input/" #temporary working directory. Change to your own path.
+if [ ! -d "$wd" ]; then
+  mkdir -p "$wd"
+else
+  rm -rf "$wd"/*
+fi
+echo "working directory created/emptied"
+
+cp ~/Feature-Selection-Qiime2/Naive_Bayes_Parameters.py "$wd"
+cp ~/Feature-Selection-Qiime2/SelectFromModel_MultinomialNB.py "$wd"
+cp ~/Feature-Selection-Qiime2/SelectFromModel_RandomForest.py "$wd"
+cp ~/Feature-Selection-Qiime2/SelectFromModel_SDG.py "$wd"
+cp ~/Feature-Selection-Qiime2/Evaluate_Method_Accuracy.py "$wd"
+cp -r ~/Feature-Selection-Qiime2/tax_credit "$wd"
+cp -r ~/tax-credit-data "$wd"
+
+cd "$wd"
 
 format_time() {
   ((h=${1}/3600))
@@ -11,19 +32,6 @@ format_time() {
   ((s=${1}%60))
   printf "%02d:%02d:%02d\n" $h $m $s
  }
- 
-source /opt/conda/etc/profile.d/conda.sh
-conda activate /home/SE/BMIG-6202-MSR/qiime2-2022.2
-
-cp -r ~/Feature-Selection-Qiime2 /scratch/metu/input/
-cp -r ~/tax-credit-data /scratch/metu/input/
-cp ~/Feature-Selection-Qiime2/Naive_Bayes_Parameters.py /scratch/metu/input
-cp ~/Feature-Selection-Qiime2/SelectFromModel_MultinomialNB.py /scratch/metu/input
-cp ~/Feature-Selection-Qiime2/SelectFromModel_RandomForest.py /scratch/metu/input
-cp ~/Feature-Selection-Qiime2/SelectFromModel_SDG.py /scratch/metu/input
-cp ~/Feature-Selection-Qiime2/Evaluate_Method_Accuracy.py /scratch/metu/input
-
-cd /scratch/metu/input
 
 python Naive_Bayes_Parameters.py \
   -r project_dir \
@@ -33,7 +41,7 @@ python Naive_Bayes_Parameters.py \
   -s results_dir\
   -f reference_database_dir
   
-echo "Naive_Bayes_Parameters.py script completed in $(format_time $SECONDS)"
+echo "Naive_Bayes_Parameters.py script completed after $(format_time $SECONDS)"
 
 python SelectFromModel_MultinomialNB.py \
   -r project_dir \
@@ -43,7 +51,7 @@ python SelectFromModel_MultinomialNB.py \
   -s results_dir\
   -f reference_database_dir
  
-echo "SelectFromModel_MultinomialNB.py script completed in $(format_time $SECONDS)"
+echo "SelectFromModel_MultinomialNB.py script completed after $(format_time $SECONDS)"
 
 python SelectFromModel_RandomForest.py\
   -r project_dir \
@@ -53,7 +61,7 @@ python SelectFromModel_RandomForest.py\
   -s results_dir\
   -f reference_database_dir
 
-echo "SelectFromModel_RandomForest.py script completed in $(format_time $SECONDS)"
+echo "SelectFromModel_RandomForest.py script completed after $(format_time $SECONDS)"
 
 python SelectFromModel_SDG.py\
   -r project_dir \
@@ -63,7 +71,7 @@ python SelectFromModel_SDG.py\
   -s results_dir\
   -f reference_database_dir
   
-echo "SelectFromModel_SDG.py script completed in $(format_time $SECONDS)"
+echo "SelectFromModel_SDG.py script completed after $(format_time $SECONDS)"
  
 python Evaluate_Method_Accuracy.py\
   -r project_dir \
@@ -73,7 +81,8 @@ python Evaluate_Method_Accuracy.py\
   -o mock_dir\
   -u outdir
  
- echo "Evaluate_Method_Accuracy.py script completed in $(format_time $SECONDS)"
+ echo "Evaluate_Method_Accuracy.py script completed after $(format_time $SECONDS)"
+ echo "That means this whole script took $(format_time $SECONDS) to run"
  echo "I'm Done"
  
 
