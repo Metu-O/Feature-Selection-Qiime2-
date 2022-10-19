64652451922e3dc836d85b005e83aee2ea96dd35
Metu-O <70252129+Metu-O@users.noreply.github.com>
2022-10-19 17:36:20 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=30Gb
-#SBATCH --output=metu_trial.out
+#SBATCH --output=script.out
 #SBATCH --time=60:00:00
  
 source /opt/conda/etc/profile.d/conda.sh
